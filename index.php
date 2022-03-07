65d14130db955aec79fab0a4d607b5d2ed6b631a
natrajbetz <101139865+natrajbetz@users.noreply.github.com>
2022-03-08 01:13:25 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,225 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Funtoss online services</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <style>
+        .header {
+            background-color: #C8BA0F;
+            text-align: center;
+        }
+
+        .carousel-caption {
+            margin-bottom: 228px;
+        }
+
+        .our-service img {
+            display: inline !important;
+            border: none !important;
+            box-shadow: none !important;
+            height: 1em !important;
+            width: 1em !important;
+            margin: 0 0.07em !important;
+            vertical-align: -0.1em !important;
+            background: none !important;
+            padding: 0 !important;
+        }
+
+        footer {
+            background-color: #3F55A5;
+        }
+
+        .contacts {
+            display: inline;
+            text-align: center;
+        }
+
+        .contacts a {
+            margin-right: 140px;
+        }
+
+        .diamond{
+            display: inline;
+        }
+        .card2{
+            background-color: #0807FF;
+            color: white;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="header" style="background-color: #C8BA0F;"><b>Our website is only for 18+ users, Please leave the website if you are from Telangana, Orissa, Assam, Sikkim, and Nagaland. This website is for Daily Fantasy Sports.</b></div>
+    <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+        <!-- <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div> -->
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="./WhatsApp Image 2022-03-07 at 7.46.20 PM.jpeg" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <h2>ONLINE ID And Fantasy <br />Sports Platform</h2>
+                    <a href="https://wa.me/+919311611742">
+                    <img src="./whatsapp.gif" alt=""></a>
+                </div>
+            </div>
+        </div>
+        <!-- <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button> -->
+        <br><br>
+    </div>
+    <div class="row">
+    <div class="col-md-4">
+      <div class="card2" style="min-height:312px;">
+        <div class="frontside">
+          <div class="card2-body project-planning text-center">
+            <h4 class="card2-title" style="background:rgba(0,0,0,0.5); margin:30px">Trustable online book</h4>
+            <div class="card-description">
+              <p> Reddy Anna Exchange Is Currently India's Biggest & No.1 Online Book.</p>
+              <a href="https://wa.me/+919311611742" class="btn btn-warning">Get ID</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="card2" style="min-height:312px;">
+        <div class="frontside">
+          <div class="card2-body project-site-visit text-center">
+            <h4 class="card2-title" style="background:rgba(0,0,0,0.5); margin:30px">FASTEST REFILL & WITHDRAWAL</h4>
+            <p>withdraw within 9 minutes<br/>We Offers Light Speed Deposit and Withdrawal Facility Of Unlimited Amount.</p>
+            <a href="https://wa.me/+919311611742" class="btn btn-warning">Get ID</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="card2" style="min-height:312px;">
+        <div class="frontside">
+          <div class="card2-body designanddevelopment text-center">
+            <h4 class="card2-title" style="background:rgba(0,0,0,0.5); margin:30px">MINIMUM ID OF 100</h4>
+            <p>Keeping In Mind Small Limit Punters, Reddy Anna Exchange Offers a Min ID Of 100 Rs Also.</p>
+            <a href="https://wa.me/+919311611742" class="btn btn-warning">Get ID</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="card2" style="min-height:312px;">
+        <div class="frontside">
+          <div class="card2-body testing text-center">
+            <h4 class="card2-title" style="background:rgba(0,0,0,0.5); margin:30px">MOST TRUSTED & RELIABLE BOOK</h4>
+            <p>Reddy Anna Exchange Works In A Very Transparent Way i.e; We are Maintaining The Trust Of More Than 12000+ Clients Base Right Now.</p>
+            <a href="https://wa.me/+919311611742" class="btn btn-warning">Get ID</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="card2" style="min-height:312px;">
+        <div class="frontside">
+          <div class="card2-body strongknitservice text-center">
+            <h4 class="card2-title" style="background:rgba(0,0,0,0.5); margin:30px">24*7 CUSTOMER SERVICE AVAILABLE</h4>
+            <p>Our Customer Executive Team Is Very Professional i.e; Customer Can Get Full Guidance in Creating A Sports ID Or Withdrawal. </p>
+            <a href="https://wa.me/+919311611742" class="btn btn-warning">Get ID</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6">
+                <img src="./default-banner-1024x480.jpg" width="550px" height="258px" alt="">
+            </div>
+            <div class="col-lg-6">
+                <h1>NEW YEAR FESTIVAL OFFER</h1>
+                <p style="color: #0807FF;
+    font-family: Rozha One, Sans-serif;
+    font-weight: 800;
+    text-shadow: 0px 0px 10px rgb(0 0 0 / 30%); font-size:29px;">Welcome Bonus On First Deposit</p>
+                <a href="https://wa.me/+919311611742"></a><img src="./download__1_-removebg-preview.png" alt=""></a>
+            </div>
+        </div><br><br>
+        <div class="our-service">
+            <h3 style="text-align: center;">Our service is our passion..<br />
+                <br />"हमारी सर्विस ही हमारी पहचान है"
+            </h3>
+            <br>
+            <p style="text-align: center;"><img draggable="false" role="img" class="emoji" alt="👉" src="https://s.w.org/images/core/emoji/13.1.0/svg/1f449.svg"> 24 HOUR आपकी सेवा में हाज़िर<br><img draggable="false" role="img" class="emoji" alt="☑️" src="https://s.w.org/images/core/emoji/13.1.0/svg/2611.svg"> हमारी सर्विसेज ही हमारी पहचान है – <img draggable="false" role="img" class="emoji" alt="💯" src="https://s.w.org/images/core/emoji/13.1.0/svg/1f4af.svg"></p>
+        </div>
+        <div class="row">
+            <div class="col-lg-6">
+                <img src="./banner.png" width="550px" height="420px" alt="">
+            </div>
+            <div class="col-lg-6"><br><br><br><br>
+                <h3>Trustable Online ID Platform</h3><br>
+                <ul>
+                    <li>Welcome Bonus 5% After Every Refill 2% for Lifetime</li>
+                    <li>24 hours instant deposit instant withdrawal</li>
+                    <li>24 hour at your Service</li>
+                </ul>
+                <br>
+                <a href="https://wa.me/+919311611742" class="btn btn-success" role="button">
+                    Whatsapp
+                </a>
+            </div>
+        </div><br><br>
+        <div class="row">
+            <div class="col-sm-6 col-md-6 col-xs-6">
+                <div class="card" style="width: 18rem;">
+                    <img class="card-img-top" src="./WhatsApp Image 2022-03-07 at 9.55.56 PM.jpeg" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">Diamond Exchange</h5>
+                        <p class="card-text">ID: Demo009<br />
+
+                            Password: Asdf1234<br />
+
+                            <a href="https://diamondexch247.co/m/login" class="card-link">click here to login</a></p>
+                    </div>
+                </div>
+            </div>
+                <div class="col-sm-6 col-md-6 col-xs-6">
+                    <div class="card" style="width: 18rem;">
+                        <img class="card-img-top" src="./WhatsApp Image 2022-03-07 at 9.59.39 PM.jpeg" alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title">Earth Betz</h5>
+                            <p class="card-text">ID: DEMOHKE<br />
+
+                                Password: Asdf1234<br />
+
+                                <a href="https://earthbetz.com/" class="card-link">click here to login</a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <br><br><br><br><br><br><br>
+            <footer class="fixed-bottom">
+                <div class="contacts row">
+                    <center>
+                        <a href="tel:+918287223467" style="text-decoration:none; color:white; font-family:-webkit-pictograph"><i aria-hidden="true" class="fa fa-phone" style="font-size:52px;color:white;width:42px;"></i></a>
+
+                        <a href="https://wa.me/+919311611742" style="text-decoration:none; color:white; font-family:-webkit-pictograph"><i aria-hidden="true" class="fa fa-whatsapp" style="font-size:52px;color:white;width:42px;"></i></a>
+
+                        <a href="https://t.me/NatrajBetz" style="text-decoration:none; color:white; font-family:-webkit-pictograph"><i aria-hidden="true" class="fa fa-telegram" style="font-size:52px;color:white;width:42px;"></i></a>
+                    </center>
+                </div>
+            </footer>
+        </div>
+</body>
+
+</html>
